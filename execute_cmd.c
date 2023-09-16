f087f1caee7f1916fd1311c60df0d495ea088d8a
Ahmed <ahmedewida221510@gmail.com>
2023-09-16 19:08:31 +0300

    add execute_cmd

---
+++ execute_cmd.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+void execute_cmd(char **argv)
+{
+    pid_t child;
+    int status;
+
+    child = fork();
+     
+    if(child == -1)
+    {
+    perror("Error happened in forking");
+    }
+    if (!child)
+    {
+    if (execvp(argv[0], argv))
+    perror ("error executing command\n");
+    exit(1);
+    }
+    else
+    {
+        waitpid(-1, &status , 0 );
+    }
+    
+}
