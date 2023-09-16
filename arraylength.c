bc462c5ace5177506b8888b336855ef0a729b232
FOXAYA <omniasaged111@gmail.com>
2023-09-16 13:17:01 -0700

    updated

--- arraylength.c
+++ arraylength.c
@@ -8,12 +8,12 @@
 
 int str_array_length(char **array)
 {
-    int length = 0;
+	int length = 0;
 
-    while (array[length] != NULL)
-    {
-        length++;
-    }
+	while (array[length] != NULL)
+	{
+		length++;
+	}
 
-    return(length);
+	return (length);
 }
