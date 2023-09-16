8fb1eb60f91eb00d193b32b7b7cca902cfa05e90
Ahmed <ahmedewida221510@gmail.com>
2023-09-16 10:13:19 +0300

    add safe_malloc

---
+++ safe_malloc.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+void *safe_malloc(size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == NULL)
+    {
+        printf("Error: malloc failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    return ptr;
+}
