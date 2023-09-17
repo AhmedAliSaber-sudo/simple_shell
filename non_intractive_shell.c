cd80f89310e1f85de081f270367d78c6e2a738b7
FOXAYA <omniasaged111@gmail.com>
2023-09-17 06:30:38 -0700

    updated

--- non_intractive_shell.c
+++ non_intractive_shell.c
@@ -2,16 +2,16 @@
 
 void non_interactive(void)
 {
-    char *line;
-    char **cmds;
+	char *line;
+	char **cmds;
 
-    while (1)
-    {
-        line = get_line();
-        cmds = parse_line(line);
-        execute_cmd(cmds);
-
-        free(line);
-        free_double_ptr(cmds, str_array_length(cmds));
-    }
-}
+	while (1)
+	{
+		line = get_line();
+		cmds = parse_line(line);
+		execute_cmd(cmds);
+		
+		free(line);
+		free_double_ptr(cmds, str_array_length(cmds));
+	}
+55555555555555555}
--- shell.c
+++ shell.c
@@ -2,14 +2,11 @@
 
 /**
  * main - the entr point of the simple shell.
- * @argc: the number of arguments passed to the shell.
- * @argv: an array of pointers to the arguments.
  * Return: 0 on success.
 */
 
-int main()
+int main(void)
 {
-    run();
-
-    return (0);
+	run();
+	return (0);
 }
