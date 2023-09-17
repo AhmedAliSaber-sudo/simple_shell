7ac8ea2b58cd18a427cfe7d30f7285c920f13f88
Ahmed <ahmedewida221510@gmail.com>
2023-09-17 12:05:16 +0300

    add non_interactive_shell

--- non_intractive_shell.c
+++ non_intractive_shell.c
@@ -5,10 +5,13 @@ void non_interactive(void)
     char *line;
     char **cmds;
 
-    line = get_line();
-    cmds = parse_line(line);
-    execute_cmd(cmds);
+    while (1)
+    {
+        line = get_line();
+        cmds = parse_line(line);
+        execute_cmd(cmds);
 
-    free(line);
-    free_double_ptr(cmds, str_array_length(cmds)); 
+        free(line);
+        free_double_ptr(cmds, str_array_length(cmds));
+    }
 }
