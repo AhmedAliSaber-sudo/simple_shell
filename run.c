da3daad860173ccf3e30e5c5569ff49c25aabc3d
FOXAYA <omniasaged111@gmail.com>
2023-09-17 05:23:34 -0700

    updated

--- run.c
+++ run.c
@@ -1,37 +1,23 @@
 #include "shell.h"
 
-<<<<<<< HEAD
-/**
- * run - function run.c:5: ERROR: trailing whitespac
-void run(void)
-{
-	while (1)
-	{
-		char *line;
-		char **cmds;
 
-		print_prompt();
-		line = get_line();
-		cmds = parse_line(line);
-		execute_cmd(cmds)
-		putchar('\n');
-		free(line);
-		free_double_ptr(cmds, str_array_length(cmds));
-	}
-=======
+/**
+ *run - function run the shell in or non interactive mood
+ *@argc : number of array
+ *@argv : array of arrays
+ */
+
 void run(int argc, char **argv)
 {
-    (void) argc;
-    (void) argv;
+	(void) argc;
+	(void) argv;
 
-    if (isatty(STDIN_FILENO) == 1)
-    {
-        interactive();
-    }
-    else
-    {
-        non_interactive();
-    }
-  
->>>>>>> 540d1b2661d14b067b9659c9febfd4bf806761ed
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		interactive();
+	}
+	else
+	{
+		non_interactive();
+	}
 }
