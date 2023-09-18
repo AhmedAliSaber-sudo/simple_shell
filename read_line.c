8f6e9d3c5718572b67df3b37325b913a007e7fb7
FOXAYA <omniasaged111@gmail.com>
2023-09-18 05:27:59 -0700

    updated

--- read_line.c
+++ read_line.c
@@ -2,32 +2,31 @@
 
 /**
 * get_line - gets the command from the user.
-* Return: the the line that the user inputs.
+* Return: the user input.
 */
 
 char *get_line()
 {
-	char *line = NULL;
-	size_t bufsize = 0;
-	size_t characters;
+    char *line = NULL;
+    size_t bufsize = 0;
+    size_t characters;
 
-	characters = getline(&line, &bufsize, stdin);
+    characters = getline(&line, &bufsize, stdin);
 
 
-	if (characters == -1)
-	{
-		if (feof(stdin))
-		{
-			free(line);
-			exit(EXIT_SUCCESS);
-		}
-		else
-		{
-			free(line);
-			perror("error while get line");
-			exit(EXIT_FAILURE);
-		}
-	}
-	return (line);
+    if (characters == -1)
+    {
+        if (feof(stdin))
+        {
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            free(line);
+            perror("error while get line");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return (line);
 }
-
