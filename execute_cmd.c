e128b7564f4e51304f0454ecb3152339a8541266
FOXAYA <omniasaged111@gmail.com>
2023-09-18 05:37:01 -0700

    updated

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,25 +1,30 @@
 #include "shell.h"
 
+/**
+* execute_cmd - executes commands.
+* @argv: array of arguments.
+*/
+
 void execute_cmd(char **argv)
 {
-	pid_t child;
-	int status;
+    pid_t child;
+    int status;
 
-	child = fork();
-	if (child == -1)
-	{
-		perror("Error happened in forking");
-	}
-	if (!child)
-	{
-		if (execvp(argv[0], argv))
-		{
-			perror(argv[0]);
-			exit(1);
-		}
-	}
-	else
-	{
-		waitpid(-1, &status, 0);
-	}
+    child = fork();
+    if (child == -1)
+    {
+        perror("Error happened in forking");
+    }
+    if (!child)
+    {
+        if (execvp(argv[0], argv))
+        {
+            perror(argv[0]);
+            exit(1);
+        }
+    }
+    else
+    {
+        waitpid(-1, &status, 0);
+    }
 }
