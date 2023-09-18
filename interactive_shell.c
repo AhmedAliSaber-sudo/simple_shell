0466f166444117bf62d827dcbde81a42ff921783
FOXAYA <omniasaged111@gmail.com>
2023-09-18 07:44:46 -0700

    updated

--- interactive_shell.c
+++ interactive_shell.c
@@ -6,17 +6,17 @@
 
 void interactive(void)
 {
-    char *line;
-    char **cmds;
+	char *line;
+	char **cmds;
 
-    while (1)
-    {
-        print_prompt();
-        line = get_line();
-        cmds = parse_line(line);
-        execute_cmd(cmds);
-        write(1, "\n", 1);
-        free(line);
-        free_double_ptr(cmds, str_array_length(cmds));
-    }
+	while (1)
+	{
+		print_prompt();
+		line = get_line();
+		cmds = parse_line(line);
+		execute_cmd(cmds);
+		write(1, "\n", 1);
+		free(line);
+		free_double_ptr(cmds, str_array_length(cmds));
+	}
 }
