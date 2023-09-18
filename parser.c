400a72294f8dce2c44f17e105f13fc91f4bad9f4
FOXAYA <omniasaged111@gmail.com>
2023-09-18 05:30:34 -0700

    updated

--- parser.c
+++ parser.c
@@ -1,38 +1,42 @@
 #include "shell.h"
 
 /**
- * parse_line - parse the line entered
+ * parse_line - parse the entered line.
  * @line: the user input.
- * Return: return array of strings.
+ * Return: return an array of strings.
 */
 
 char **parse_line(char *line)
 {
-	const char *delim = " \n";
-	char *line_copy = NULL;
-	char *token;
-	char **tokens;
-	int tokens_numbers = 0;
-	int i;
+    const char *delim = " \n";
+    char *line_copy = NULL;
+    char *token;
+    char **tokens;
+    int tokens_numbers = 0;
+    int i;
 
-	line_copy = safe_malloc(sizeof(char *) * strlen(line));
-	strcpy(line_copy, line);
-	token = strtok(line_copy, delim);
+    line_copy = safe_malloc(sizeof(char *) * strlen(line));
 
-	while (token)
-	{
-		tokens_numbers++;
-		token = strtok(NULL, delim);
-	}
-	tokens_numbers++;
-	tokens = safe_malloc(sizeof(char *) * tokens_numbers);
-	token = strtok(line, delim);
-	for (i = 0; token != NULL; i++)
-	{
-		tokens[i] = strdup(token);
-		token = strtok(NULL, delim);
-	}
-	tokens[i] = NULL;
-	free(line_copy);
-	return (tokens);
+    strcpy(line_copy, line);
+    token = strtok(line_copy, delim);
+
+    while (token)
+    {
+        tokens_numbers++;
+        token = strtok(NULL, delim);
+    }
+    tokens_numbers++;
+
+    tokens = safe_malloc(sizeof(char *) * tokens_numbers);
+
+    token = strtok(line, delim);
+    for (i = 0; token != NULL; i++)
+    {
+        tokens[i] = strdup(token);
+        token = strtok(NULL, delim);
+    }
+    tokens[i] = NULL;
+
+    free(line_copy);
+    return (tokens);
 }
--- print_array.c
+++ print_array.c
@@ -1,16 +1,15 @@
 #include "shell.h"
 
 /**
- *print_string_array - array of string
- *@arr : pointer to array
+ *print_string_array - prints an array of string.
+ *@arr : pointer to array.
  */
 
 void print_string_array(char **arr)
 {
-	while (*arr)
-	{
-		printf("%s\n", *arr);
-		arr++;
-	}
-
+    while (*arr)
+    {
+        printf("%s\n", *arr);
+        arr++;
+    }
 }
