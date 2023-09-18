27e07b4d94cd6b40a8118916db3b34cfacc4bea7
FOXAYA <omniasaged111@gmail.com>
2023-09-18 07:54:47 -0700

    updated

--- read_line.c
+++ read_line.c
@@ -7,26 +7,25 @@
 
 char *get_line()
 {
-    char *line = NULL;
-    size_t bufsize = 0;
-    size_t characters;
+	char *line = NULL;
+	size_t bufsize = 0;
+	size_t characters;
 
-    characters = getline(&line, &bufsize, stdin);
+	characters = getline(&line, &bufsize, stdin);
 
-
-    if (characters == -1)
-    {
-        if (feof(stdin))
-        {
-            free(line);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            free(line);
-            perror("error while get line");
-            exit(EXIT_FAILURE);
-        }
-    }
-    return (line);
+	if (characters == -1)
+	{
+		if (feof(stdin))
+		{
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			free(line);
+			perror("error while get line");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
 }
