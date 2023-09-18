fa679ee312ea91d9596232437eb755ee71c891f8
FOXAYA <omniasaged111@gmail.com>
2023-09-18 07:48:20 -0700

    updated

--- parser.c
+++ parser.c
@@ -8,35 +8,35 @@
 
 char **parse_line(char *line)
 {
-    const char *delim = " \n";
-    char *line_copy = NULL;
-    char *token;
-    char **tokens;
-    int tokens_numbers = 0;
-    int i;
+	const char *delim = " \n";
+	char *line_copy = NULL;
+	char *token;
+	char **tokens;
+	int tokens_numbers = 0;
+	int i;
 
-    line_copy = safe_malloc(sizeof(char *) * strlen(line));
+	line_copy = safe_malloc(sizeof(char *) * strlen(line));
 
-    strcpy(line_copy, line);
-    token = strtok(line_copy, delim);
+	strcpy(line_copy, line);
+	token = strtok(line_copy, delim);
 
-    while (token)
-    {
-        tokens_numbers++;
-        token = strtok(NULL, delim);
-    }
-    tokens_numbers++;
+	while (token)
+	{
+		tokens_numbers++;
+		token = strtok(NULL, delim);
+	}
+	tokens_numbers++;
 
-    tokens = safe_malloc(sizeof(char *) * tokens_numbers);
+	tokens = safe_malloc(sizeof(char *) * tokens_numbers);
 
-    token = strtok(line, delim);
-    for (i = 0; token != NULL; i++)
-    {
-        tokens[i] = strdup(token);
-        token = strtok(NULL, delim);
-    }
-    tokens[i] = NULL;
+	token = strtok(line, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		tokens[i] = strdup(token);
+		token = strtok(NULL, delim);
+	}
+	tokens[i] = NULL;
 
-    free(line_copy);
-    return (tokens);
+	free(line_copy);
+	return (tokens);
 }
