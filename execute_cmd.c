1aa72cbbe6a01c577897dfbee5528ed7af009721
FOXAYA <omniasaged111@gmail.com>
2023-09-18 07:42:37 -0700

    updated

--- execute_cmd.c
+++ execute_cmd.c
@@ -7,24 +7,24 @@
 
 void execute_cmd(char **argv)
 {
-    pid_t child;
-    int status;
+	pid_t child;
+	int status;
 
-    child = fork();
-    if (child == -1)
-    {
-        perror("Error happened in forking");
-    }
-    if (!child)
-    {
-        if (execvp(argv[0], argv))
-        {
-            perror(argv[0]);
-            exit(1);
-        }
-    }
-    else
-    {
-        waitpid(-1, &status, 0);
-    }
+	child = fork();
+	if (child == -1)
+	{
+		perror("Error happened in forking");
+	}
+	if (!child)
+	{
+		if (execvp(argv[0], argv))
+		{
+			perror(argv[0]);
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(-1, &status, 0);
+	}
 }
