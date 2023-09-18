d6609c5d53581b7670ca72dad5743c7b4355d75c
FOXAYA <omniasaged111@gmail.com>
2023-09-18 05:35:10 -0700

    updated

--- free_double_pointer.c
+++ free_double_pointer.c
@@ -1,20 +1,19 @@
 #include "shell.h"
 
 /**
- * free_double_ptr - free double pointer.
- * @arr : pointer to a pointer.
- * @size : the size of the pointer.
+ * free_double_ptr - frees a double pointer.
+ * @arr: an array.
+ * @size: the size of the array.
  */
 
 void free_double_ptr(char **arr, size_t size)
 {
-	size_t i;
+    size_t i;
 
-	for (i = 0; i < size; i++)
-	{
-		free(arr[i]);
-	}
+    for (i = 0; i < size; i++)
+    {
+        free(arr[i]);
+    }
 
-	free(arr);
+    free(arr);
 }
-
