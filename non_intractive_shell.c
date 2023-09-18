4cff1d5249e83a22b37a9dd269ab4cb7e2d39be1
FOXAYA <omniasaged111@gmail.com>
2023-09-18 07:30:31 -0700

    updated

--- non_intractive_shell.c
+++ non_intractive_shell.c
@@ -1,16 +1,20 @@
 #include "shell.h"
 
+/**
+* non_interactive - runs the shell in non-interactive mode.
+*/
+
 void non_interactive(void)
 {
-	char *line;
-	char **cmds;
+    char *line;
+    char **cmds;
 
-	while (1)
-	{
-		line = get_line();
-		cmds = parse_line(line);
-		execute_cmd(cmds);
-		free(line);
-		free_double_ptr(cmds, str_array_length(cmds));
-	}
+    while (1)
+    {
+        line = get_line();
+        cmds = parse_line(line);
+        execute_cmd(cmds);
+        free(line);
+        free_double_ptr(cmds, str_array_length(cmds));
+    }
 }
