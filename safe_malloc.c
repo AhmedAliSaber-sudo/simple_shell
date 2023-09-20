faeff263164f702ef4da420e7cc31a87e8f1ae26
Ahmed <ahmedewida221510@gmail.com>
2023-09-20 12:28:32 +0300

    add description for safe_malloc function

--- safe_malloc.c
+++ safe_malloc.c
@@ -1,12 +1,19 @@
 #include "shell.h"
 
+/**
+* safe_malloc - allocate memory and retuen error if failed.
+* @size: the size of the memory to be allocated.
+* Return: the pointer to the allocated memory.
+*/
+
 void *safe_malloc(size_t size)
 {
 	void *ptr = malloc(size);
+	char *message = "Error: malloc failed.\n";
 
 	if (ptr == NULL)
 	{
-		printf("Error: malloc failed.\n");
+		write(1, message, strlen(message));
 		exit(EXIT_FAILURE);
 	}
 
