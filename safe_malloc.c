4702ff1bee0c930fa08e8e190162972f20aac996
FOXAYA <omniasaged111@gmail.com>
2023-09-20 13:43:28 -0700

    updated

--- safe_malloc.c
+++ safe_malloc.c
@@ -3,15 +3,7 @@
 /**
 * safe_malloc - allocate memory and return error if failed.
 * @size: the size of the memory to be allocated.
-* Return: the pointer to the allocated memory.
-*/
-
-void *safe_malloc(size_t size)
-{
-	void *ptr = malloc(size);
-	char *message = "Error: malloc failed.\n";
-
-	if (ptr == NULL)EXIT_FAILURE);
+* Return: the pointer to the alloe);
 	}
 
 	return (ptr);
