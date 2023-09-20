c6ba17451ed8e5dc5ed43d60369bb9a3802810b7
Ahmed <ahmedewida221510@gmail.com>
2023-09-20 18:54:39 +0300

    add env_set_unset

---
+++ env_set_unset.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+* set_env_var - Initialize a new environment variable,
+* or modify an existing one.
+* @variable: the variable name.
+* @value: the value of the variable.
+* Return: 0 on success, -1 if failed.
+*/
+int set_env_var(char *variable, char *value)
+{
+	if (setenv(variable, value, 1) != 0)
+	{
+		fprintf(stderr, "Failed to set environment variable\n");
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+* unset_env_var - Initialize a new environment variable,
+* or modify an existing one.
+* @variable: the variable name.
+* Return: 0 on success, -1 if failed.
+*/
+
+int unset_env_var(char *variable)
+{
+	if (unsetenv(variable) != 0)
+	{
+		fprintf(stderr, "Failed to unset environment variable\n");
+		return (-1);
+	}
+	return (0);
+}
