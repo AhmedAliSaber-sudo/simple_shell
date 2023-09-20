b82ff102238f236b8d13693bc42a4d12b8562659
FOXAYA <omniasaged111@gmail.com>
2023-09-20 13:23:48 -0700

    updated

--- free_double_pointer.c
+++ free_double_pointer.c
@@ -6,14 +6,6 @@
  * @size: the size of the array.
  */
 
-void free_double_ptr(char **arr, size_t size)
-{
-	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		free(arr[i]);
-	}
-
+void free_double_ptr(
 	free(arr);
 }
