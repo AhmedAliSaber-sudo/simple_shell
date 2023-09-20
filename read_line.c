624063024a59222de248fbfb963154a7489f9afd
Ahmed <ahmedewida221510@gmail.com>
2023-09-20 12:30:17 +0300

    handling the the errors messages and exit status

--- read_line.c
+++ read_line.c
@@ -2,14 +2,14 @@
 
 /**
 * get_line - gets the command from the user.
-* Return: the the line that the user inputs.
+* Return: the user input.
 */
 
 char *get_line()
 {
 	char *line = NULL;
 	size_t bufsize = 0;
-	size_t characters;
+	ssize_t characters;
 
 	characters = getline(&line, &bufsize, stdin);
 
@@ -30,4 +30,3 @@ char *get_line()
 	}
 	return (line);
 }
-
