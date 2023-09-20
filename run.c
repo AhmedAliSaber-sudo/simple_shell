2fb159aa25ec5e00503a44d67bd7448058c56f23
Ahmed <ahmedewida221510@gmail.com>
2023-09-20 12:29:05 +0300

    add the description

--- run.c
+++ run.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
-
 /**
- *run - function run the shell in or non interactive mood
- *@argc : number of array
- *@argv : array of arrays
+ *run - runs the shell in interactive or non-interactive mode.
+ *@argc: the number of arguments.
+ *@argv: array of strings.
  */
 
 void run(int argc, char **argv)
