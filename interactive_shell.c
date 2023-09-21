75304eb4322c3df64c0c01a294addbcd0c42d775
Ahmed <ahmedewida221510@gmail.com>
2023-09-21 06:28:41 +0300

    update

--- interactive_shell.c
+++ interactive_shell.c
@@ -12,19 +12,10 @@ void interactive(void)
 	while (1)
 	{
 		print_prompt();
-		line = get_line();
-		if (line == NULL)
-		{
-			free(line);
-			break;
-		}
 
+		line = get_line();
 		cmds = parse_line(line);
-		if (cmds == NULL)
-		{
-			free(cmds);
-			break;
-		}
+
 		if (strcmp(cmds[0], "exit") == 0)
 		{
 			exit_shell(cmds);
@@ -32,12 +23,12 @@ void interactive(void)
 		}
 
 		execute_cmd(cmds);
+
 		write(1, "\n", 1);
 
 		free(line);
 		free_double_ptr(cmds, str_array_length(cmds));
 	}
-
 	free(line);
 	free_double_ptr(cmds, str_array_length(cmds));
 }
